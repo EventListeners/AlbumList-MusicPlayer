f4ab442da2ff3d4f152fc2b951a27ada26266ef1
Andrew MacDonald <MAC@Andrews-MacBook-Pro-4.local>
2018-09-27 06:49:31 -0700

    API extended to support all CRUD ops

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const { getArtist } = require('../database/index.js');
+const Artist = require('../database/index.js');
 const cors = require('cors');
 const server = express();
 
@@ -10,10 +10,37 @@ server.use(cors());
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static(path.join(__dirname, '../public')));
 
-server.get('/artists/albums/:artistID', (req, res) => {
-  getArtist(req.params.artistID, data => {
-    res.send(data);
-  });
+
+server.get('/artists/:artistID', (req, res) => {
+  Artist.find({'artistID': req.params.id})
+    .then(artist => res.json(artist))
+    .catch(err => console.log(err));
 });
+
+
+server.post('/artists', (req, res) => {
+  let newArtist = new Artist(req.body);
+  newArtist.save()
+    .then(() => res.status(201).json({message:"Successfully Added"}))
+    .catch(() => res.status(400).end(error));
+});
+
+
+server.put('/artists/:artistID', (req, res) => {
+  Artist.updateOne({ artistID: req.params.id }, { $set: req.body })
+    .then(res.json({message:"Successfully Updated"}))
+    .catch(() => res.status(400).end(error));
+});
+
+
+server.delete('/artists/:artistID', (req, res) => {
+  Artist.deleteOne({ artistID: req.params.id })
+    .then(res.json({message:"Successfully Deleted"}))
+    .catch(() => res.status(400).end(error));
+});
+
+
+
+
 
 module.exports = server;
